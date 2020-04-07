fd6d5315e36fc6d311c1b4e9c011d6eb28e4b0eb
chamindikaLaksarani <63224389+Chami133@users.noreply.github.com>
2020-04-07 17:59:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route("/",methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route("/dropdown", methods=['POST'])
+def dropdown():
+    global selectValue
+    selectValue = request.form.get('val', None)
+    print(selectValue)
+    if selectValue == None:
+        print("No value")
+        return render_template("index.html",selectValue=selectValue)
+    return selectValue
+
+
+if __name__ == '__main__':
+    app.run()
+
+value = "LK"
